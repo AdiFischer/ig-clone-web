a078654b95932516f43665271a71af8b5b798362
AdiFischer <adifischer1@gmail.com>
2023-01-04 12:22:07 -0500

    minor fixes

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,28 +7,28 @@ import Post from "./Post"
 
 export default function Feed() {
   const [photoList, setPhotoList] = useState()
-  const [showUpload, setShowUpload] =  useState(false)
+  const [showUpload, setShowUpload] = useState(false)
   useEffect(() => {
     fetch('https://express-ts-af.web.app/photos')
-    // fetch('http://localhost:5002/photos')
-    .then(results => results.json())
-    .then(data => setPhotoList(data))
-    .catch(alert)
+      // fetch('http://localhost:5002/photos')
+      .then(results => results.json())
+      .then(data => setPhotoList(data))
+      .catch(alert)
   }, [setPhotoList])
-    return (
-       
-          <section className="photo-feed">
-           {!photoList
-           ? <p>Loading...</p>
-           : photoList.map(post => (
-            <Post setPhotoList={setPhotoList} post={post} key={post.photoId} />
-           ))
-          }
-          {showUpload ? <UploadModal setPhotoList={setPhotoList} setShowUpload={setShowUpload}/> : null}
-          {/* {showUpload &&} */}
-          <Button onClick={() => setShowUpload(true)} 
-          className="fab" type="primary" 
-          shape="circle" size="large">+</Button>
-          </section>
-    )   
-    }
+  return (
+
+    <section className="photo-feed">
+      {!photoList
+        ? <p>Loading...</p>
+        : photoList.map(post => (
+          <Post setPhotoList={setPhotoList} post={post} key={post.photoId} />
+        ))
+      }
+      {showUpload ? <UploadModal setPhotoList={setPhotoList} setShowUpload={setShowUpload} /> : null}
+      {/* {showUpload &&} */}
+      <Button onClick={() => setShowUpload(true)}
+        className="fab" type="primary"
+        shape="circle" size="large">+</Button>
+    </section>
+  )
+}
